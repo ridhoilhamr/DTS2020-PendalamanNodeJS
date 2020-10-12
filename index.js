befadc0c95187409063947b11d97b59cf843ca03
ridhoilhamr <ridhoilhamr111@gmail.com>
2020-10-13 00:28:55 +0700

    init body parser

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import hbs from 'hbs'
 import path from 'path'
 import morgan from 'morgan'
+import bodyParser from 'body-parser'
 
 const __dirname = path.resolve()
 
@@ -15,6 +16,9 @@ app.engine('html', hbs.__express)
 // log incoming reques
 app.use(morgan('combined'))
 
+// parse request body
+app.use(bodyParser.urlencoded())
+
 // serve static file
 app.use('/assets', express.static(__dirname + '/assets'))
 // app.use('/files', express.static(__dirname + '/files'))
@@ -25,6 +29,18 @@ app.get('/', (req, res, next)=>{
 
 app.get('/product', async (req, res, next) => {
     res.render('product')
+})
+
+
+// handle form GET method
+app.get('/add-product', (req, res, next) => {
+  res.send(req.query)
+})
+
+// handle form POST method
+app.post('/add-product', (req, res, next) => {
+  console.log('Request', req.body)
+  res.send(req.body)
 })
 
 //send error message
